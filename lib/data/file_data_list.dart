29be0684f74ac4bf2c4bd39f60eddc66ab020e0f
dart practice <mahjabinsuhani663@gmail.com>
2026-01-12 01:23:39 +0600

    added

--- lib/data/file_data_list.dart
+++ lib/data/file_data_list.dart
@@ -1,10 +1,5 @@
-import 'package:noteappwithfirebase/models/folder_data_model.dart';
-import 'package:flutter/material.dart';
-import 'package:noteappwithfirebase/views/academic_folder_screen.dart';
-import 'package:noteappwithfirebase/views/folder_screen.dart';
 import '../models/file_data_model.dart';
-import '../views/others_folder_screen.dart';
-import '../views/work_folder_screen.dart';
+
 class FileDataList {
   static List<FileDataModel> fileList=[
     FileDataModel(
--- lib/data/folder_data_list.dart
+++ lib/data/folder_data_list.dart
@@ -1,10 +1,5 @@
 import 'package:noteappwithfirebase/models/folder_data_model.dart';
 import 'package:flutter/material.dart';
-import 'package:noteappwithfirebase/views/academic_folder_screen.dart';
-import 'package:noteappwithfirebase/views/folder_screen.dart';
-
-import '../views/others_folder_screen.dart';
-import '../views/work_folder_screen.dart';
 class FolderDataList {
   static List<FolderDataModel> folderList=[
       FolderDataModel(
