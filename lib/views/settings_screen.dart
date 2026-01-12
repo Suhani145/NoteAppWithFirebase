a95136bb8d9e913225cdfa6552617ec17bd7fea3
dart practice <mahjabinsuhani663@gmail.com>
2026-01-12 21:13:17 +0600

    added

--- lib/views/settings_screen.dart
+++ lib/views/settings_screen.dart
@@ -13,60 +13,69 @@ class SettingsScreen extends StatelessWidget {
       style: TextStyle(decoration: TextDecoration.none),
       child: Container(
         color: screenBG,
-        child: Column(
-          children: [
-            SizedBox(height: responsiveSize.screenHeight*0.1),
-             SizedBox(
-               width: responsiveSize.screenWidth*0.3,
-               height: responsiveSize.screenHeight*0.15,
-               child: Card(
-                 elevation: 0,
-                 color: white,
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadiusGeometry.circular(100)
-                 ),
-                 child: Icon(Icons.person, size: 40, color: iconColor),
-               ),
-             ),
-            SizedBox(height: responsiveSize.screenHeight*0.1),
-            SizedBox(
-              height: responsiveSize.screenHeight*0.1,
-              width: responsiveSize.screenWidth*0.9,
-              child: Card(
-                color: white,
-                 elevation: 2,
-                 shadowColor: baseColor,
-                 child: Padding(
-                   padding: const EdgeInsets.all(8.0),
-                   child: Column(
-                     crossAxisAlignment: CrossAxisAlignment.start,
-                     mainAxisSize: MainAxisSize.min,
-                     children: [
-                       Text("User Name",
-                       style: TextStyle(
-                         fontWeight: FontWeight.bold,
-                         fontSize: 18
-                       )),
-                       Text("username@gmail.com",
-                           style: TextStyle(
-                               fontSize: 16
-                           ))
-                     ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: responsiveSize.screenHeight*0.05),
+               IconButton(onPressed: (){Get.back();}, icon: Icon(Icons.arrow_back_ios, color: baseColor)),
+               Center(
+                 child: SizedBox(
+                   width: responsiveSize.screenWidth*0.3,
+                   height: responsiveSize.screenHeight*0.15,
+                   child: Card(
+                     elevation: 0,
+                     color: white,
+                     shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadiusGeometry.circular(100)
+                     ),
+                     child: Icon(Icons.person, size: 40, color: iconColor),
                    ),
-                 )
-              ),
-            ),
-            SizedBox(height: responsiveSize.screenHeight*0.05),
-            ElevatedButton(onPressed: (){
-              Get.to(OnboardingScreen());
-            },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: baseColor,
-                  foregroundColor: white,
+                 ),
+               ),
+              SizedBox(height: responsiveSize.screenHeight*0.1),
+              SizedBox(
+                height: responsiveSize.screenHeight*0.1,
+                width: responsiveSize.screenWidth*0.9,
+                child: Card(
+                  color: white,
+                   elevation: 2,
+                   shadowColor: baseColor,
+                   child: Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       mainAxisSize: MainAxisSize.min,
+                       children: [
+                         Text("User Name",
+                         style: TextStyle(
+                           fontWeight: FontWeight.bold,
+                           fontSize: 18
+                         )),
+                         Text("username@gmail.com",
+                             style: TextStyle(
+                                 fontSize: 16
+                             ))
+                       ],
+                     ),
+                   )
                 ),
-                child:
-            Text("Log Out"))
-          ],
+              ),
+              SizedBox(height: responsiveSize.screenHeight*0.05),
+              Center(
+                child: ElevatedButton(onPressed: (){
+                  Get.to(OnboardingScreen());
+                },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: baseColor,
+                      foregroundColor: white,
+                    ),
+                    child:
+                Text("Log Out")),
+              )
+            ],
+          ),
         ),
       ),
     );
